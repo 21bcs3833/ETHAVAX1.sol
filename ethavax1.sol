e5adce46ab098993a56d33c08d38f181c4c4a827
21bcs3833 <136236240+21bcs3833@users.noreply.github.com>
2023-08-04 11:02:44 +0530

    Create ethavax1.sol

---
+++ ethavax1.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract mysalary {
+
+    uint public totalmysalaryrecived;
+    mapping(address => bool) public hasmysalarygiven;
+
+    function mysalarygiven() public {
+        require(!hasmysalarygiven[msg.sender], "Already Given"); 
+
+        totalmysalaryrecived++;
+        hasmysalarygiven[msg.sender] = true;
+
+        if (totalmysalaryrecived > 50) {
+            revert("employee limit exceeded"); 
+        }
+
+        assert(totalmysalaryrecived <= 50); 
+    }
+}
